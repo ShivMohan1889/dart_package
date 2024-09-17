db4ab6db6369cc2fd96b1813c16605b2bd1168fb
Shiv Mohan Singh <shiv@tribondinfosystems.com>
2024-09-17 17:27:31 +0530

    update return type

--- lib/src/audit/pdf_generator/tb_audit_pdf_generator.dart
+++ lib/src/audit/pdf_generator/tb_audit_pdf_generator.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:dart_pdf_package/src/audit/dto/audit_assessment_dto.dart';
 import 'package:dart_pdf_package/src/audit/dto/audit_assessment_question_dto.dart';
@@ -56,7 +57,7 @@ class TbAuditPdfGenerator {
   /// this list holds the  AuditImage for the given Audit Assessment
   final List<AuditImageDto> auditImageList = List.empty(growable: true);
 
-  Future<pw.Document> generatePDF() async {
+  Future<Uint8List> generatePDF() async {
     await preparePdfs(auditAssessmentEntity!);
     final pdf = pw.Document();
 
@@ -146,7 +147,7 @@ class TbAuditPdfGenerator {
     final file = File(aPath);
     var data = await pdf.save();
     file.writeAsBytesSync(data);
-    return pdf;
+    return data;
   }
 
   // void generate({
