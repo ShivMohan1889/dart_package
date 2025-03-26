b3a2a65d451782377625df851701d7feb716ee47
Shiv Mohan Singh <shiv@tribondinfosystems.com>
2025-03-26 14:04:08 +0530

    fixed ms issue in which content was splitting header and statrmetn on diffent pages

--- lib/src/ms/tb_ms_pdf_generator.dart
+++ lib/src/ms/tb_ms_pdf_generator.dart
@@ -235,30 +235,46 @@ class TbMsPdfGenerator {
     required Context context,
   }) {
     for (HeaderRows headerRow in pdfData.headers) {
-      createHeaderWidgets(
+      List<Widget> headerWidgets = createHeaderWidgets(
         headerRow: headerRow,
         context: context,
       );
+      headerWidget.addAll(headerWidgets);
     }
     print(headerWidget);
   }
+
+  // void iterateHeadersForPdfWidets({
+  //   required MsPdfData pdfData,
+  //   required Context context,
+  // }) {
+  //   for (HeaderRows headerRow in pdfData.headers) {
+  //     createHeaderWidgets(
+  //       headerRow: headerRow,
+  //       context: context,
+  //     );
+  //   }
+  //   print(headerWidget);
+  // }
 
 /* ************************************** */
 //  CREATE HEADER WIDGET
   /// responsible to iterate headers /statements and create widgets
 /* ************************************** */
 
-  void createHeaderWidgets({
+  List<Widget> createHeaderWidgets({
     required HeaderRows headerRow,
     required Context context,
   }) {
+    List<Widget> result = [];
+
+    // Create the header widget
     Widget widget = MsPdfCustomText(
       text: headerRow.name,
       padding: headerRow.level == 0
           ? TbMsPdfPaddings.paddingForTbMsHeaderEntityHeaderLevelZero
           : TbMsPdfPaddings.paddingForTbMsHeaderEntityHeaderLevelNotZero,
       textStyle: headerRow.level == 0
-          // ? msPdfTextStyle.msMainHeaderTextStyle()
           ? TbPdfHelper().textStyleGenerator(
               font: Theme.of(context).header0.fontBold,
               color: TbMsPdfColors.black,
@@ -270,24 +286,38 @@ class TbMsPdfGenerator {
               fontSize: 11,
             ),
     );
+
     if (widget is MsPdfCustomText) {
       widget.rowType = 1;
     }
 
-    headerWidget.add(widget);
+    // Add the header to our result list
+    result.add(widget);
 
+    // Now handle the different cases for what follows the header
     if ((headerRow.headerRows ?? []).isNotEmpty) {
-      for (HeaderRows headerRow in headerRow.headerRows ?? []) {
-        createHeaderWidgets(
-          headerRow: headerRow,
+      // Process subheaders recursively
+      for (HeaderRows subHeader in headerRow.headerRows ?? []) {
+        List<Widget> subHeaderWidgets = createHeaderWidgets(
+          headerRow: subHeader,
           context: context,
         );
+        result.addAll(subHeaderWidgets);
       }
     } else if ((headerRow.hazardIcons ?? []).isNotEmpty) {
-      showMsStatementIconsOnPdf(hazardIconData: headerRow.hazardIcons ?? []);
+      // Add hazard icons
+      List<Widget> iconRows = [];
+      showMsStatementIconsOnPdf(
+        hazardIconData: headerRow.hazardIcons ?? [],
+        outputList: iconRows,
+      );
+      result.addAll(iconRows);
     } else if ((headerRow.statements ?? []).isNotEmpty) {
+      // For statements, we'll wrap the header with the first statement
+      List<Widget> statementWidgets = [];
+
       for (HeaderStatementData statementData in headerRow.statements ?? []) {
-        Widget w = MsStatementRow(
+        Widget statementWidget = MsStatementRow(
           statementName: statementData.text,
           statmentTextStyle: TbPdfHelper().textStyleGenerator(
             font: Theme.of(context).header0.font,
@@ -295,18 +325,41 @@ class TbMsPdfGenerator {
             fontSize: 11,
           ),
         );
-        headerWidget.add(w);
+        statementWidgets.add(statementWidget);
 
         if ((statementData.memoryImages ?? []).isNotEmpty) {
-          headerWidget.addAll(
+          statementWidgets.addAll(
             showStatementImagesOnPdf(
               images: statementData.memoryImages ?? [],
             ),
           );
         }
       }
+
+      // If there are statements, take the first one and group it with the header
+      if (statementWidgets.isNotEmpty) {
+        // Remove the header from result as we'll add it back grouped
+        result.removeLast();
+
+        // Create a group with header and first statement
+        Widget headerWithFirstStatement = Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            widget, // The header
+            statementWidgets.first, // The first statement
+          ],
+        );
+
+        result.add(headerWithFirstStatement);
+
+        // Add remaining statements (if any)
+        if (statementWidgets.length > 1) {
+          result.addAll(statementWidgets.sublist(1));
+        }
+      }
     }
 
+    // Add any header images
     if ((headerRow.images ?? []).isNotEmpty) {
       List<MemoryImage> images = [];
 
@@ -315,15 +368,93 @@ class TbMsPdfGenerator {
           images.add(image.memoryImage!);
         }
       }
-      headerWidget.addAll(
+
+      result.addAll(
         showStatementImagesOnPdf(
           images: images,
         ),
       );
     }
-  } 
 
-   /* *********************************** / 
+    return result;
+  }
+
+  // void createHeaderWidgets({
+  //   required HeaderRows headerRow,
+  //   required Context context,
+  // }) {
+  //   Widget widget = MsPdfCustomText(
+  //     text: headerRow.name,
+  //     padding: headerRow.level == 0
+  //         ? TbMsPdfPaddings.paddingForTbMsHeaderEntityHeaderLevelZero
+  //         : TbMsPdfPaddings.paddingForTbMsHeaderEntityHeaderLevelNotZero,
+  //     textStyle: headerRow.level == 0
+  //         // ? msPdfTextStyle.msMainHeaderTextStyle()
+  //         ? TbPdfHelper().textStyleGenerator(
+  //             font: Theme.of(context).header0.fontBold,
+  //             color: TbMsPdfColors.black,
+  //             fontSize: 12,
+  //           )
+  //         : TbPdfHelper().textStyleGenerator(
+  //             font: Theme.of(context).header0.fontBold,
+  //             color: TbMsPdfColors.black,
+  //             fontSize: 11,
+  //           ),
+  //   );
+  //   if (widget is MsPdfCustomText) {
+  //     widget.rowType = 1;
+  //   }
+
+  //   headerWidget.add(widget);
+
+  //   if ((headerRow.headerRows ?? []).isNotEmpty) {
+  //     for (HeaderRows headerRow in headerRow.headerRows ?? []) {
+  //       createHeaderWidgets(
+  //         headerRow: headerRow,
+  //         context: context,
+  //       );
+  //     }
+  //   } else if ((headerRow.hazardIcons ?? []).isNotEmpty) {
+  //     showMsStatementIconsOnPdf(hazardIconData: headerRow.hazardIcons ?? []);
+  //   } else if ((headerRow.statements ?? []).isNotEmpty) {
+  //     for (HeaderStatementData statementData in headerRow.statements ?? []) {
+  //       Widget w = MsStatementRow(
+  //         statementName: statementData.text,
+  //         statmentTextStyle: TbPdfHelper().textStyleGenerator(
+  //           font: Theme.of(context).header0.font,
+  //           color: MsPdfColors.black,
+  //           fontSize: 11,
+  //         ),
+  //       );
+  //       headerWidget.add(w);
+
+  //       if ((statementData.memoryImages ?? []).isNotEmpty) {
+  //         headerWidget.addAll(
+  //           showStatementImagesOnPdf(
+  //             images: statementData.memoryImages ?? [],
+  //           ),
+  //         );
+  //       }
+  //     }
+  //   }
+
+  //   if ((headerRow.images ?? []).isNotEmpty) {
+  //     List<MemoryImage> images = [];
+
+  //     for (HeaderReferenceImageData image in headerRow.images ?? []) {
+  //       if (image.memoryImage != null) {
+  //         images.add(image.memoryImage!);
+  //       }
+  //     }
+  //     headerWidget.addAll(
+  //       showStatementImagesOnPdf(
+  //         images: images,
+  //       ),
+  //     );
+  //   }
+  // }
+
+  /* *********************************** / 
     // SHOW STATEMENT IMAGE ON PDF  
     
     /// 
@@ -382,15 +513,16 @@ class TbMsPdfGenerator {
 
   void showMsStatementIconsOnPdf({
     required List<HazardIconData> hazardIconData,
+    required List<Widget> outputList,
   }) {
     /// this list holds the memory image widget of ms Statement Icons
     List<Widget> listIconImageItems = [];
     // this holds the row widget which contain children widget as listIconImageItems
     List<Widget> listIconRow = [];
+
     // this loop for iterating the element of list of StatementIcon
     // in this loop we have implement the logic to show only 6 image in a single row and image which came after the 6th one
     // is show on next row
-    // Future.forEach(msStatementHazadIconDtoList, (iconDto) {
     for (HazardIconData iconData in hazardIconData) {
       int index = (hazardIconData).indexOf(iconData);
 
@@ -432,22 +564,86 @@ class TbMsPdfGenerator {
       }
     }
 
-    var row = MsStatementHazardIconsRow(
-      iconsImageList: listIconImageItems,
-    );
-    listIconRow.add(row);
+    // Add the last row if it has any items
+    if (listIconImageItems.isNotEmpty) {
+      var row = MsStatementHazardIconsRow(
+        iconsImageList: listIconImageItems,
+      );
+      listIconRow.add(row);
+    }
 
-    headerWidget.addAll(listIconRow);
-
-    listIconRow = [];
-
-    listIconImageItems = [];
+    // Add all rows to the output list instead of headerWidget
+    outputList.addAll(listIconRow);
   }
+
+  // void showMsStatementIconsOnPdf({
+  //   required List<HazardIconData> hazardIconData,
+  // }) {
+  //   /// this list holds the memory image widget of ms Statement Icons
+  //   List<Widget> listIconImageItems = [];
+  //   // this holds the row widget which contain children widget as listIconImageItems
+  //   List<Widget> listIconRow = [];
+  //   // this loop for iterating the element of list of StatementIcon
+  //   // in this loop we have implement the logic to show only 6 image in a single row and image which came after the 6th one
+  //   // is show on next row
+  //   // Future.forEach(msStatementHazadIconDtoList, (iconDto) {
+  //   for (HazardIconData iconData in hazardIconData) {
+  //     int index = (hazardIconData).indexOf(iconData);
+
+  //     MemoryImage? iconImage = iconData.iconMemoryImage;
+
+  //     ///logic behind the code is we get the remainder of index of iconImage image by
+  //     /// taking the modulus of index by 6
+  //     /// when remainder equals to zero we create new Widget MsStatementHazardIconRow which contains
+  //     /// the list of listIconImageItems as children and add it this new widget to the list of listIconImageRow
+  //     /// to show the on pdf page
+  //     if (index % 6 == 0) {
+  //       if (index == 0) {
+  //         if (iconImage != null) {
+  //           Widget iconWidget = MsStatementHazardIconItem(
+  //             iconImage: iconImage,
+  //           );
+  //           listIconImageItems.add(iconWidget);
+  //         }
+  //       } else {
+  //         var row = MsStatementHazardIconsRow(
+  //           iconsImageList: listIconImageItems,
+  //         );
+  //         listIconRow.add(row);
+  //         listIconImageItems = [];
+  //         if (iconImage != null) {
+  //           Widget iconWidget = MsStatementHazardIconItem(
+  //             iconImage: iconImage,
+  //           );
+  //           listIconImageItems.add(iconWidget);
+  //         }
+  //       }
+  //     } else {
+  //       if (iconImage != null) {
+  //         Widget iconWidget = MsStatementHazardIconItem(
+  //           iconImage: iconImage,
+  //         );
+  //         listIconImageItems.add(iconWidget);
+  //       }
+  //     }
+  //   }
+
+  //   var row = MsStatementHazardIconsRow(
+  //     iconsImageList: listIconImageItems,
+  //   );
+  //   listIconRow.add(row);
+
+  //   headerWidget.addAll(listIconRow);
+
+  //   listIconRow = [];
+
+  //   listIconImageItems = [];
+  // }
 
   /// ***********************************
   ///   SHOW SIGN OFF USER WITH DATA
-  /// this method is responsible showing the sign off in pdf 
-  /// ***********************************  
+  /// this method is responsible showing the sign off in pdf
+  /// ***********************************
   void showSignOffUserWithData({
     required Context context,
     required List<ReviewSignOffSignatureData> signOffUsers,
